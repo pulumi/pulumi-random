421cb634ba69dd710329c88aa3a3356a412f1ab4
Matt Ellis <matt@pulumi.com>
2019-08-23 14:41:53 -0700

    Run tfgen with latest pulumi-terraform

--- sdk/python/pulumi_random/random_integer.py
+++ sdk/python/pulumi_random/random_integer.py
@@ -6,6 +6,7 @@ import json
 import warnings
 import pulumi
 import pulumi.runtime
+from typing import Union
 from . import utilities, tables
 
 class RandomInteger(pulumi.CustomResource):
@@ -101,7 +102,7 @@ class RandomInteger(pulumi.CustomResource):
 
         > This content is derived from https://github.com/terraform-providers/terraform-provider-random/blob/master/website/docs/r/integer.html.markdown.
         """
-        opts = pulumi.ResourceOptions(id=id) if opts is None else opts.merge(pulumi.ResourceOptions(id=id))
+        opts = pulumi.ResourceOptions.merge(opts, pulumi.ResourceOptions(id=id))
 
         __props__ = dict()
         __props__["keepers"] = keepers
