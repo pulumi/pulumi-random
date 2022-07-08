fafee1eb1a33dc3c5ae42f2b2384dc929b8f8236
Paul Stack <public@paulstack.co.uk>
2022-07-08 13:59:57 +0100

    [internal] Adding support for Java SDK to the pulumi-random repo (#232)

--- provider/cmd/pulumi-resource-random/main.go
+++ provider/cmd/pulumi-resource-random/main.go
@@ -17,10 +17,15 @@
 package main
 
 import (
+	_ "embed"
+
 	random "github.com/pulumi/pulumi-random/provider/v4"
 	"github.com/pulumi/pulumi-random/provider/v4/pkg/version"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 )
+
+//go:embed schema-embed.json
+var pulumiSchema []byte
 
 func main() {
 	tfbridge.Main("random", version.Version, random.Provider(), pulumiSchema)
