1f51d7408a6d6d8a7ad480f6754fd8c1d64ec2b4
Anton Tayanovskyy <anton@pulumi.com>
2023-01-20 10:44:16 -0500

    Update to Plugin Framework v1.1.1

--- provider/cmd/pulumi-tfgen-random/main.go
+++ provider/cmd/pulumi-tfgen-random/main.go
@@ -15,9 +15,10 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-terraform-bridge/pkg/tfpfbridge/tfgen"
+
 	random "github.com/pulumi/pulumi-random/provider/v4"
 	"github.com/pulumi/pulumi-random/provider/v4/pkg/version"
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfgen"
 )
 
 func main() {
