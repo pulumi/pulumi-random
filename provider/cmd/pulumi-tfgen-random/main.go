d3ee26795fe0888654fccd986de3ddb849c53d64
Anton Tayanovskyy <anton@pulumi.com>
2023-02-07 16:05:42 -0500

    Upgrade to Plugin Framework

--- provider/cmd/pulumi-tfgen-random/main.go
+++ provider/cmd/pulumi-tfgen-random/main.go
@@ -15,11 +15,11 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-terraform-bridge/pf/tfgen"
+
 	random "github.com/pulumi/pulumi-random/provider/v4"
-	"github.com/pulumi/pulumi-random/provider/v4/pkg/version"
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfgen"
 )
 
 func main() {
-	tfgen.Main("random", version.Version, random.Provider())
+	tfgen.Main("random", random.Provider())
 }
