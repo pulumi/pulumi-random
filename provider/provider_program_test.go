b49c561eba09f3c9bdc337bd24563dfebbd0e6ac
Venelin <venelin@pulumi.com>
2024-04-08 12:01:22 +0100

    Tests preamble

---
+++ provider/provider_program_test.go
@@ -0,0 +1,62 @@
+//go:build !go && !nodejs && !python && !dotnet
+// +build !go,!nodejs,!python,!dotnet
+
+// Copyright 2016-2024, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package random
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/pulumi/providertest/pulumitest"
+	"github.com/pulumi/providertest/pulumitest/opttest"
+)
+
+const providerName = "random"
+
+func testProgram(t *testing.T, dir string) {
+	if testing.Short() {
+		t.Skipf("Skipping in testing.Short() mode, assuming this is a CI run without credentials")
+	}
+	cwd, err := os.Getwd()
+	require.NoError(t, err)
+	test := pulumitest.NewPulumiTest(t, dir,
+		opttest.LocalProviderPath(providerName, filepath.Join(cwd, "..", "bin")),
+		opttest.SkipInstall(),
+	)
+	test.Up()
+}
+
+func TestPrograms(t *testing.T) {
+	programs := []string{
+		"test-programs/index_randompassword",
+		"test-programs/index_randomstring",
+		"test-programs/index_randomid",
+		"test-programs/index_randomuuid",
+		"test-programs/index_randompet",
+		"test-programs/index_randominteger",
+		"test-programs/index_randomshuffle",
+		"test-programs/index_randombytes",
+	}
+	for _, p := range programs {
+		t.Run(p, func(t *testing.T) {
+			testProgram(t, p)
+		})
+	}
+}
