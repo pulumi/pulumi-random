dde6b361e33089556804b4122cf3dad3f0eef7f0
Graham Pedersen <Graham.Pedersen@gmail.com>
2025-11-14 11:42:15 -0800

    Committing latest sdk and schema for testing

--- sdk/go/random/randomId.go
+++ sdk/go/random/randomId.go
@@ -36,7 +36,7 @@ import (
 //
 //	"fmt"
 //
-//	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
+//	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/ec2"
 //	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
 //	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 //
--- sdk/go/random/randomPassword.go
+++ sdk/go/random/randomPassword.go
@@ -19,7 +19,7 @@ import (
 //
 // import (
 //
-//	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/rds"
+//	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/rds"
 //	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
 //	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 //
